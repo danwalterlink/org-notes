cbc644a91ff79e4461f268254be8d6f9d1d0fdbe
nick martin <nmartin84@gmail.com>
2021-01-23 21:55:23 -0600

    updates

---
+++ literature/test.el
@@ -0,0 +1,23 @@
+;;; test.el --- description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 Nick Martin
+;;
+;; Author: Nick Martin <http://github/nick>
+;; Maintainer: Nick Martin <nmartin84@gmail.com>
+;; Created: January 23, 2021
+;; Modified: January 23, 2021
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage: https://github.com/nick/test
+;; Package-Requires: ((emacs 28.0.50) (cl-lib "0.5"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  description
+;;
+;;; Code:
+
+(provide 'test)
+;;; test.el ends here
